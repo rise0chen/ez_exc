c4f4caefd08da67aac61053b7a8632c68017903c
rise0chen <rise0chen@163.com>
2024-07-27 14:07:38 +0800

    fix: mexc

--- exc-mexc/src/futures_web/http/mod.rs
+++ exc-mexc/src/futures_web/http/mod.rs
@@ -12,7 +12,18 @@ pub fn req_to_http<Req: Rest>(req: &Req, key: &Key) -> Result<Request<Body>, any
     if req.need_sign() {
         if let Some(token) = &key.web_key {
             builder = builder
-                .header("cookie", format!("u_id={}", token))
+                .header("accept", "*/*")
+                .header("accept-language", "en-US,en;q=0.9")
+                .header("origin", HOST)
+                .header("sec-ch-ua", r#"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"#)
+                .header("sec-fetch-mode", "cors")
+                .header("sec-fetch-site", "same-origin")
+                .header("sec-fetch-user", "?1")
+                .header(
+                    "user-agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+                )
+                .header("cookie", format!("uc_token={}; u_id={}", token, token))
                 .header("Authorization", token.as_str());
         }
     }
