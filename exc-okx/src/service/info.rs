0aa9eeb4fa170ae5575f578c6c313212b54d8bc8
rise0chen <rise0chen@163.com>
2025-05-27 10:48:54 +0800

    fix: okx get_funding_rate

--- exc-okx/src/service/info.rs
+++ exc-okx/src/service/info.rs
@@ -6,6 +6,9 @@ use tower::ServiceExt;
 
 impl Okx {
     pub async fn get_funding_rate(&mut self, symbol: &Symbol) -> Result<FundingRate, ExchangeError> {
+        if symbol.is_spot() {
+            return Ok(FundingRate::default());
+        }
         let symbol_id = crate::symnol::symbol_id(symbol);
         use crate::api::http::info::GetFundingRateRequest;
         let req = GetFundingRateRequest { inst_id: symbol_id };
