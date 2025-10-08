33373de52a3d95c32ebcf0d4899140d746be96bf
rise0chen <rise0chen@163.com>
2025-10-08 12:41:37 +0800

    fix: book len

--- exc-dex/src/service/book.rs
+++ exc-dex/src/service/book.rs
@@ -11,15 +11,21 @@ use exc_util::types::book::{Depth, Order};
 fn price(sqrt_price_x96: U160) -> f64 {
     (sqrt_price_x96.arithmetic_shr(32).to::<u128>() as f64 / 2.0f64.powi(64)).powi(2)
 }
-fn map_order0(x: &Cex::Order, symbol: &Symbol) -> Order {
+fn map_order0(x: &Cex::Order, symbol: &Symbol) -> Option<Order> {
+    if x.amount0 == 0 {
+        return None;
+    }
     let price = price(x.price);
     let size = format_units(x.amount0, symbol.precision as u8).unwrap();
-    Order::new(price / symbol.multi_price, size.parse().unwrap())
+    Some(Order::new(price / symbol.multi_price, size.parse().unwrap()))
 }
-fn map_order1(x: &Cex::Order, symbol: &Symbol) -> Order {
+fn map_order1(x: &Cex::Order, symbol: &Symbol) -> Option<Order> {
+    if x.amount1 == 0 {
+        return None;
+    }
     let price = 1.0 / price(x.price);
     let size = format_units(x.amount1, symbol.precision as u8).unwrap();
-    Order::new(price / symbol.multi_price, size.parse().unwrap())
+    Some(Order::new(price / symbol.multi_price, size.parse().unwrap()))
 }
 
 impl Dex {
@@ -28,12 +34,12 @@ impl Dex {
         let depth = cex.getDepth(self.pool.clone(), limit);
         let depth = depth.block(BlockId::pending()).call().await.map_err(map_err)?;
         let (bid, ask) = if self.key.pool_cfg.base_is_0 {
-            let bid = depth.bids.iter().map(|x| map_order0(x, symbol)).collect();
-            let ask = depth.asks.iter().map(|x| map_order0(x, symbol)).collect();
+            let bid = depth.bids.iter().filter_map(|x| map_order0(x, symbol)).collect();
+            let ask = depth.asks.iter().filter_map(|x| map_order0(x, symbol)).collect();
             (bid, ask)
         } else {
-            let bid = depth.asks.iter().map(|x| map_order1(x, symbol)).collect();
-            let ask = depth.bids.iter().map(|x| map_order1(x, symbol)).collect();
+            let bid = depth.asks.iter().filter_map(|x| map_order1(x, symbol)).collect();
+            let ask = depth.bids.iter().filter_map(|x| map_order1(x, symbol)).collect();
             (bid, ask)
         };
         let bid_ask = Depth {
