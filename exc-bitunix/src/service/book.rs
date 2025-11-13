4d385a497b6570ce68b42ad510ce3f1a0bf12a3e
rise0chen <rise0chen@163.com>
2025-11-14 07:02:29 +0800

    fix: bitunix depth limit

--- exc-bitunix/src/service/book.rs
+++ exc-bitunix/src/service/book.rs
@@ -6,13 +6,16 @@ use time::OffsetDateTime;
 use tower::ServiceExt;
 
 impl Bitunix {
-    pub async fn get_depth(&mut self, symbol: &Symbol, limit: u16) -> Result<Depth, ExchangeError> {
+    pub async fn get_depth(&mut self, symbol: &Symbol, _limit: u16) -> Result<Depth, ExchangeError> {
         let symbol_id = crate::symnol::symbol_id(symbol);
         let bid_ask = if symbol.is_spot() {
             todo!();
         } else {
             use crate::futures_api::http::book::GetDepthRequest;
-            let req = GetDepthRequest { symbol: symbol_id, limit };
+            let req = GetDepthRequest {
+                symbol: symbol_id,
+                limit: 15,
+            };
             let resp = self.oneshot(req).await?;
             Depth {
                 bid: resp.bids.iter().map(|x| Order::new(x.0, x.1)).collect(),
