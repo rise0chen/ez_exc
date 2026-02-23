03807fb651210cba2ffcd993847ecbf5fce53bee
rise0chen <rise0chen@163.com>
2026-02-23 21:29:57 +0800

    feat: dydx oracle_price

--- exc-dydx/src/service/book.rs
+++ exc-dydx/src/service/book.rs
@@ -1,6 +1,7 @@
 use super::Dydx;
 use bigdecimal::ToPrimitive;
 use dydx::indexer::types::OrderbookResponsePriceLevel;
+use dydx::indexer::ListPerpetualMarketsOpts;
 use exc_core::ExchangeError;
 use exc_util::symbol::Symbol;
 use exc_util::types::book::{Depth, Order};
@@ -14,10 +15,19 @@ impl Dydx {
     pub async fn get_depth(&mut self, symbol: &Symbol, _limit: u16) -> Result<Depth, ExchangeError> {
         let symbol_id = crate::symnol::symbol_id(symbol);
         let resp = self.indexer().markets().get_perpetual_market_orderbook(&symbol_id).await?;
+        let opts = Some(ListPerpetualMarketsOpts {
+            ticker: Some(symbol_id.clone()),
+            limit: None,
+        });
+        let mut markets = self.indexer().markets().get_perpetual_markets(opts).await?;
+        let price = markets.remove(&symbol_id).unwrap().oracle_price;
+        let price = price.map(|x| x.to_f64().unwrap()).unwrap_or_default();
+        let bid_order = [Order::new(price * 0.999, 50.0 / price)];
+        let ask_order = [Order::new(price * 1.001, 50.0 / price)];
 
         Ok(Depth {
-            bid: resp.bids.iter().map(order).collect(),
-            ask: resp.asks.iter().map(order).collect(),
+            bid: bid_order.into_iter().chain(resp.bids.iter().map(order)).collect(),
+            ask: ask_order.into_iter().chain(resp.asks.iter().map(order)).collect(),
             version: (OffsetDateTime::now_utc().unix_timestamp_nanos() / 1_000_000) as u64,
         })
     }
