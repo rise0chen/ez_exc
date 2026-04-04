832889a3b7a6b50fba43b800e438d0963926d437
rise0chen <rise0chen@163.com>
2026-04-04 09:00:48 +0800

    fix: lighter

--- exc-lighter/src/service/book.rs
+++ exc-lighter/src/service/book.rs
@@ -10,7 +10,9 @@ impl Lighter {
         let bid_ask = if symbol.is_spot() {
             todo!();
         } else {
-            let resp = self.ws.get_order_book(&symbol.base_id).await.unwrap();
+            let Some(resp) = self.ws.get_order_book(&symbol.base_id).await else {
+                return Ok(Depth::default());
+            };
             let mut bids = BTreeMap::new();
             let mut asks = BTreeMap::new();
             resp.bids.into_iter().for_each(|x| {
