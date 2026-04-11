a955b19fa54542cc3403936b8c0aff5ca53a8035
rise0chen <rise0chen@163.com>
2026-04-11 23:10:08 +0800

    fix: hyperliquid spot

--- exc-hyperliquid/src/service/info.rs
+++ exc-hyperliquid/src/service/info.rs
@@ -6,6 +6,9 @@ use time::{Duration, OffsetDateTime};
 
 impl Hyperliquid {
     pub async fn get_index_price(&mut self, symbol: &Symbol) -> Result<f64, ExchangeError> {
+        if symbol.is_spot() {
+            return Ok(0.0);
+        }
         let coin = crate::symnol::symbol_id(symbol);
         if let Some(ch) = self.ws.index_prices.get(&coin) {
             Ok(*ch.borrow())
@@ -15,6 +18,9 @@ impl Hyperliquid {
     }
 
     pub async fn get_funding_rate(&mut self, symbol: &Symbol) -> Result<FundingRate, ExchangeError> {
+        if symbol.is_spot() {
+            return Ok(FundingRate::default());
+        }
         let coin = crate::symnol::symbol_id(symbol);
         let rate = if let Some(ch) = self.ws.funding_rates.get(&coin) {
             *ch.borrow()
@@ -30,6 +36,9 @@ impl Hyperliquid {
         })
     }
     pub async fn get_funding_rate_history(&mut self, symbol: &Symbol, day: u8) -> Result<Vec<FundingRate>, ExchangeError> {
+        if symbol.is_spot() {
+            return Ok(Vec::new());
+        }
         let start_time = ((OffsetDateTime::now_utc() - Duration::days(day as i64)).unix_timestamp_nanos() / 1_000_000) as u64;
         let coin = crate::symnol::symbol_id(symbol);
         let mut resp = self.http.funding_history(coin, start_time, None).await?;
