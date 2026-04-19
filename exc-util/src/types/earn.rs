b2e85a5587b4863a025a262cb535ead8e40b0534
rise0chen <rise0chen@163.com>
2026-04-19 10:33:59 +0800

    perf: st rate

--- exc-util/src/types/earn.rs
+++ exc-util/src/types/earn.rs
@@ -14,6 +14,11 @@ pub fn common_st_rate(symbol: &Symbol) -> Option<StRate> {
             start_time: 0,
             apy: 0.0,
         },
+        "XAUT0" => StRate {
+            rate: 0.9975,
+            start_time: 0,
+            apy: 0.0,
+        },
         "PAXG" => StRate {
             rate: 0.99875,
             start_time: 0,
